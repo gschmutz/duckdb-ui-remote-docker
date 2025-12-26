5542e71df7459ae100d672cc6f59aba193d62d41
Guido Schmutz <schmutz68@gmail.com>
2025-12-26 21:57:26 +0100

    add support for init

--- startup.sh
+++ startup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Run DuckDB UI in screen
-screen -L -Logfile ./duckdb_ui.log -dmS duckdb_ui /usr/local/bin/duckdb -ui
+if [[ -n "${DUCKDB_INIT_SCRIPT:-}" ]]; then
+  echo "Starting DuckDB UI with init script: $DUCKDB_INIT_SCRIPT"
+
+  screen -L -Logfile ./duckdb_ui.log -dmS duckdb_ui /usr/local/bin/duckdb -init $DUCKDB_INIT_SCRIPT -ui
+else
+  echo "Starting DuckDB UI without init script"
+
+  screen -L -Logfile ./duckdb_ui.log -dmS duckdb_ui /usr/local/bin/duckdb -ui
+fi
 
 # Run HAProxy
 screen -L -Logfile ./haproxy.log -dmS haproxy_app haproxy -f /usr/local/etc/haproxy/haproxy.cfg
